a2faf66fe0bca295a19b7a5ce1ff955e122f2aa8
Shah-Wajahat-IU <shahwajahatijaz@gmail.com>
2021-12-22 22:48:23 +0500

    to complete the task of save value in db

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . views import index
+from . views import index,addTodoForm
 urlpatterns = [
    
     path("",index,name="index"),
+    
+    path("add",addTodoForm,name="add"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django import forms
+import django
+from django.shortcuts import render,redirect
 from .models import TodoList
+from .forms import TodoListForm
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
     todo_list=TodoList.objects.order_by('id')
-    context={"todo_list":todo_list}
+    form= TodoListForm()
+    
+    context={"todo_list":todo_list,'form':form}
     return render(request,"todolist/index.html",context)
+@require_POST
+def addTodoForm(request):
+    form= TodoListForm(request.POST)
+    if form.is_valid():
+        new_todo=TodoList(text=request.POST['text'])
+        new_todo.save()
+    print(request.POST['text'])
+
+    return redirect('index')
