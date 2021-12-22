8dcd8a0335756286c55b2343a5b07b9793099bf8
Shah-Wajahat-IU <shahwajahatijaz@gmail.com>
2021-12-22 22:01:33 +0500

    show data on html and apply if conditions

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import TodoList
 
 # Create your views here.
 def index(request):
-    return render(request,"todolist/index.html",{})
+    todo_list=TodoList.objects.order_by('id')
+    context={"todo_list":todo_list}
+    return render(request,"todolist/index.html",context)
